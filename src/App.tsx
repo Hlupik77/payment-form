5f71cd62ccf337ac343562c95eb25d56d1638119
aleksandr <kra@trusted.ru>
2025-08-14 11:32:07 +0300

    feat: Added config in index.css and added font Inter

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
       <div className="text-3xl font-bold text-center underline">
-          <h1 className="text-title text-primary">Привет</h1>
+          <h1 className="text-error ">Привет</h1>
           <button className="text-button bg-success text-white">Кнопка</button>
       </div>
   )
